ffa6e0ad039d0a2a596dcbbaa42af5bfc831df1c
Thomas Bnt <thomasbnt@users.noreply.github.com>
2017-02-20 20:30:04 +0100

    Envoyer un message en privé directement à l'user.
    Send MP directly.

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 // Discord.js
 var Discord = require("discord.js");
-// Node-opus & opusscript sont pour l'audio, donc pour ce bot il n'est pas nécessaire de l'activer
+// Node-opus & opusscript sont pour l'audio, donc pour ce bot, il n'est pas nécessaire de l'activer
 //require("node-opus"); 
 //require("opusscript");
 
@@ -15,9 +15,9 @@ let prefix = "/";
 bot.on('ready', () => {
 
   bot.user.setStatus("dnd"); //dnd , online , ldle
-  bot.user.setGame("<Your Game Here", "https://twitch.tv/tonusername"); // Pour les streamers , vous êtes obligé de mettre une URL Twitch sinon cela ne marchera pas.
-  // Attention ! " bot.user.setGame " peux être utilisé qu'une seule fois, donc vérifiez si il y en a qu'un seul.
-  //bot.user.setGame("In Dev");
+  //bot.user.setGame("Ton Jeu Ici", "https://twitch.tv/myusernametwitch"); // Pour les streamers , vous êtes obligé de mettre une URL Twitch sinon cela ne marchera pas.
+  bot.usersetGame("Besoin d'aide? /help");
+  // Attention ! " bot.user.setGame " peux être utilisé qu'une seule fois, donc vérifiez si il y en a qu'un seul. 
   console.log("Houston ! Le bot est en ligne");
 });
 
@@ -26,7 +26,7 @@ bot.on("message", msg => {
 
       // Command /help
      if (msg.content.startsWith(prefix + "help")) {
-      msg.channel.sendMessage("```List of commands : \n\n /version  | Version  \n /help | All Commands  \n    ```");
+      msg.channel.sendMessage("```List of commands : \n\n /version  | Version  \n /help | All Commands  \n\n /mpmoi | Un message privé comme test.   ```");
       console.log("Command executed: /help")
     }
     // Command /version
@@ -34,10 +34,15 @@ bot.on("message", msg => {
       msg.channel.sendMessage("``` Bot Discord Basic - Version 1.0 \n Creator : Thomas Bnt | @Hyprimort#9520 \n GitHub :https://github.com/thomasbnt/bot-discord-basic-js  | ⚠  Please not erase the author name ```");
       console.log("Command executed : /version")
     }
+    // Command /mpmoi
+    if (msg.content.startsWith(prefix + "mpmoi")){
+      msg.author.sendMessage("Bonjour Houston !")
+      console.log("Command executed : /mpmoi")
+    }
 
 });
 
 
 
 //Login with TOKEN for Bot
-bot.login(" VOTRE TOKEN ICI ");
+bot.login("VOTRE TOKEN ICI SANS ESPACE");
